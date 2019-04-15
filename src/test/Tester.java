1dd5256580ae5261646797c14d5aca34bb6e8721
jayasithu <jayasithuh@gmail.com>
2019-04-15 16:41:18 +0530

    Flow network

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -3,9 +3,20 @@ package test;
 import com.FlowEdge;
 import com.FlowNetwork;
 import com.FordFulkerson;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer;
+import org.apache.commons.collections15.Transformer;
 
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 /*
 This Class was crated to test the graph using different inputs
@@ -27,6 +38,51 @@ public class Tester {
         System.out.println(f);
 
         FordFulkerson maxFlow = new FordFulkerson(f,0,vertices-1);
+
+
+        Graph<Integer, FlowEdge> g = new SparseMultigraph<>();
+
+        for (int i = 0; i < vertices; i++) {
+            g.addVertex(i);
+        }
+
+        for (FlowEdge e : edges) {
+            g.addEdge(e, e.getFrom(), e.getTo());
+        }
+
+        Layout<Integer, FlowEdge> layout = new CircleLayout(g);
+        BasicVisualizationServer<Integer, FlowEdge> vv =
+                new BasicVisualizationServer<>(layout);
+
+        vv.setPreferredSize(new Dimension(450, 450));
+        Transformer<Integer, Paint> vertexPaint = new Transformer<Integer, Paint>() {
+            public Paint transform(Integer i) {
+                return Color.GREEN;
+            }
+        };
+
+        final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f );
+        Transformer<FlowEdge, Stroke> edgeStrokeTransformer =
+                new Transformer<>() {
+                    public Stroke transform(FlowEdge s) {
+                        return edgeStroke;
+                    }
+                };
+
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+        vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
+        JFrame frame = new JFrame("Flow Network");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(vv);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
 
         System.out.println("The Maximum Flow of the Graph : "+maxFlow.value());
         long time = maxFlow.getT2() - maxFlow.getT1()/1000;
